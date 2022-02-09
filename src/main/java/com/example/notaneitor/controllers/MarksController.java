c8d941866dff7489d149473aabe32f7d21dd2f33
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-02-09 21:57:31 +0100

    guardado

--- src/main/java/com/example/notaneitor/controllers/MarksController.java
+++ src/main/java/com/example/notaneitor/controllers/MarksController.java
@@ -3,9 +3,10 @@ package com.example.notaneitor.controllers;
 import com.example.notaneitor.entities.Mark;
 import com.example.notaneitor.services.MarksService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class MarksController {
     @Autowired //Inyectar el servicio
     private MarksService marksService;
