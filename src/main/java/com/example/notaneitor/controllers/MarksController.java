844949f270e3c7d88b031213fc388ee0d23c5206
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-02-09 19:40:37 +0100

    SDI-2122-107-2.3-Beans, servicios e inyección de dependencias.

---
+++ src/main/java/com/example/notaneitor/controllers/MarksController.java
@@ -0,0 +1,33 @@
+package com.example.notaneitor.controllers;
+
+import com.example.notaneitor.entities.Mark;
+import com.example.notaneitor.services.MarksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MarksController {
+    @Autowired //Inyectar el servicio
+    private MarksService marksService;
+
+    @RequestMapping("/mark/list")
+    public String getList() {
+        return marksService.getMarks().toString();
+    }
+    @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
+    public String setMark(@ModelAttribute Mark mark) {
+        marksService.addMark(mark);
+        return "Ok";
+    }
+    @RequestMapping("/mark/details/{id}")
+    public String getDetail(@PathVariable Long id) {
+        return marksService.getMark(id).toString();
+    }
+
+    @RequestMapping("/mark/delete/{id}")
+    public String deleteMark(@PathVariable Long id){
+        marksService.deleteMark(id);
+
+        return "Ok";
+    }
+}
