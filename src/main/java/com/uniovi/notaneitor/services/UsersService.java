56b90dac1d4ac9ddf7e00009e5137ef17ff92bed
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-02-16 19:44:58 +0100

    BeanCurrentlyInCreationException

--- src/main/java/com/uniovi/notaneitor/services/UsersService.java
+++ src/main/java/com/uniovi/notaneitor/services/UsersService.java
@@ -8,14 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.uniovi.notaneitor.entities.*;
 import com.uniovi.notaneitor.services.UsersService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class UsersService {
     @Autowired
     private UsersRepository usersRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @PostConstruct
     public void init() {
     }
+
     public List<User> getUsers() {
         List<User> users = new ArrayList<User>();
         usersRepository.findAll().forEach(users::add);
@@ -25,9 +31,16 @@ public class UsersService {
     public User getUser(Long id) {
         return usersRepository.findById(id).get();
     }
+
     public void addUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         usersRepository.save(user);
     }
+
+    public User getUserByDni(String dni) {
+        return usersRepository.findByDni(dni);
+    }
+
     public void deleteUser(Long id) {
         usersRepository.deleteById(id);
     }
