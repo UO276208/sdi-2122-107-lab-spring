1bccd670f3c7d3b2c46ef61d7da1c891e1bd3aa6
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-02-16 18:16:56 +0100

    SDI-2122-107-3.3-Acceso a datos con entidades relacionadas.

--- src/main/java/com/uniovi/notaneitor/entities/Mark.java
+++ src/main/java/com/uniovi/notaneitor/entities/Mark.java
@@ -11,12 +11,23 @@ public class Mark {
     private String description;
     private Double score;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     public Mark(){    }
 
     public Mark(Long id, String description, Double score) {
         this.id = id;
         this.description = description;
         this.score = score;
+    }
+
+    public  Mark(String description, Double score, User user){
+        super();
+        this.description = description;
+        this.score = score;
+        this.user = user;
     }
 
     public Long getId() {
@@ -44,4 +55,11 @@ public class Mark {
         return "Mark{" + "id=" + id + ", description='" + description + '\'' + ", score=" + score + '}';
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
