5648a110fc46c52cca2352f87623d01a64e73b79
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-02-27 21:12:38 +0100

    SDI-2122-107-4.5-SpringBoot-Búsqueda

--- src/main/java/com/uniovi/notaneitor/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/notaneitor/repositories/MarksRepository.java
@@ -11,6 +11,12 @@ import java.util.List;
 
 public interface MarksRepository extends CrudRepository<Mark, Long>{
 
+    @Query("SELECT r FROM Mark r WHERE(LOWER(r.description)LIKE LOWER(?1) or LOWER(r.user.name) LIKE LOWER(?1))")
+    List<Mark> searchByDescriptionAndName (String searchtext);
+
+    @Query("SELECT r FROM  Mark r WHERE(LOWER(r.description) LIKE lower(?1) OR lower(r.user.name) like lower(?1)) and r.user = ?2")
+    List<Mark> searchByDescriptionNameAndUser(String searchtext, User user);
+
     @Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC")
     List<Mark> findAllByUser(User user);
 
