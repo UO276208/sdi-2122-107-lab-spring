528abe6e6334a125bea2a383f0c598e3645da86f
uo276208 <uo276208@uniovi.es>
2022-03-09 10:03:13 +0100

    SDI-2122-107-sesión5-ejercicio-complementario1

--- src/test/java/com/uniovi/notaneitor/pageobjects/PO_PrivateView.java
+++ src/test/java/com/uniovi/notaneitor/pageobjects/PO_PrivateView.java
@@ -1,10 +1,13 @@
 package com.uniovi.notaneitor.pageobjects;
 
 import com.uniovi.notaneitor.util.SeleniumUtils;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class PO_PrivateView extends PO_NavView {
     static public void fillFormAddMark(WebDriver driver, int userOrder, String descriptionp, String scorep)
@@ -23,5 +26,20 @@ public class PO_PrivateView extends PO_NavView {
         score.sendKeys(scorep);
         By boton = By.className("btn");
         driver.findElement(boton).click();
+    }
+
+    static public void login(WebDriver driver, String dnip, String passwordp){
+        //Vamos al formulario de logueo.
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+        //Rellenamos el formulario
+        PO_LoginView.fillLoginForm(driver, "99999988F", "123456");
+
+        PO_View.checkElementBy(driver, "text", dnip);
+    }
+
+    static public void logout(WebDriver driver){
+        //Ahora nos desconectamos y comprobamos que aparece el menú de registrarse
+        String loginText = PO_HomeView.getP().getString("signup.message", PO_Properties.getSPANISH());
+        PO_PrivateView.clickOption(driver, "logout", "text", loginText);
     }
 }
